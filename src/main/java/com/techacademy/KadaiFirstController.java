30d68be466b6ecfde9c656e1fb4e5da99b86b8df
takeshi.hisatomi <take_tomy7@yahoo.co.jp>
2024-11-14 22:24:34 +0900

    KadaiFirstSpringBoot

---
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -0,0 +1,67 @@
+package com.techacademy;
+
+import java.util.Calendar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KadaiFirstController {
+
+    @GetMapping("dayofweek/{date}")
+    public String dispDayOfWeek(@PathVariable String date) {
+        String year = date.substring(0, 4);
+        String month = date.substring(4, 6);
+        String day = date.substring(6, 8);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Integer.parseInt(year), Integer.parseInt(month) - 1, Integer.parseInt(day));
+
+        switch (cal.get(Calendar.DAY_OF_WEEK)) {
+        case Calendar.SUNDAY:
+            return "Sunday";
+        case Calendar.MONDAY:
+            return "Monday";
+        case Calendar.TUESDAY:
+            return "Tuesday";
+        case Calendar.WEDNESDAY:
+            return "Wednesday";
+        case Calendar.THURSDAY:
+            return "Thursday";
+        case Calendar.FRIDAY:
+            return "Friday";
+        case Calendar.SATURDAY:
+            return "Saturday";
+        }
+        return "NoDayOfWeek";
+    }
+
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果:" + res;
+    }
+
+    @GetMapping("/minus/{val1}/{val2}")
+    public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 - val2;
+        return "計算結果:" + res;
+    }
+
+    @GetMapping("/times/{val1}/{val2}")
+    public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 * val2;
+        return "計算結果:" + res;
+    }
+
+    @GetMapping("/divid/{val1}/{val2}")
+    public String calcDivid(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 / val2;
+        return "計算結果:" + res;
+    }
+}
